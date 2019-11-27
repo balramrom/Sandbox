c9a90b5b3599bab087aaa4cdcae373f0f17d4680
balramrom <balramrom95@gmail.com>
2019-11-27 16:43:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contacts-fis2019
+
+This repository contains the example code for the proyect that we use in the course FIS
---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Datastore = require('nedb');
+
+var port = 3000;
+var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname + "/contacts.json";
+
+console.log("Starting API Server...");
+
+var app = express();
+app.use(bodyParser.json());
+
+var db = new Datastore({
+    filename:DB_FILE_NAME,
+    autoload:true
+});
+
+app.get("/", (req,res)=>{
+    res.send("<html><body><h1>Hello World</h1></body></html>");
+});
+
+app.get(BASE_API_PATH + "/contacts", (req,res)=>{
+    console.log(Date() + " - GET /contacts");
+    db.find({}, (err,contacts)=>{
+        if(err){
+            console.log(Date() + "  - " + err)
+        }else{
+            res.send(contacts.map((contact)=>{
+                delete contact._id;
+                return contact;
+            }));
+        }
+    });
+});
+
+app.post(BASE_API_PATH + "/contacts", (req,res)=>{
+    console.log(Date() + " - POST /contacts");
+    var contact = req.body;
+    db.insert(contact, (err)=>{
+        if(err){
+            console.log(Date() + " - " + err);
+        }else{
+            res.sendStatus(201);
+        }
+    }); 
+});
+
+app.listen(port);
+
+console.log("Server ready!");
